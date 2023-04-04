41cf9064bbdd03c06f8495767fb3286aad85fd56
cleisonsantos <css.cleison@outlook.com>
2023-04-04 05:21:03 -0300

    add - implementação inicial do bulljs.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotasFiscaisModule } from './notas-fiscais/notas-fiscais.module';
 import { ContribuinteModule } from './contribuinte/contribuinte.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [NotasFiscaisModule, ContribuinteModule],
+  imports: [
+    NotasFiscaisModule,
+    ContribuinteModule,
+    BullModule.forRoot({
+      redis: {
+        host: process.env['REDIS_HOST'],
+        port: +process.env['REDIS_PORT'],
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
